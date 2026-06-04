c54b7628869f43446480652aefc25e643540940b
janosbabik <janos.babik@ess.eu>
2026-06-04 18:23:34 +0200

    feat: update manifest link in index.html and refactor ReactDOM rendering in index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,14 @@
 import { StrictMode } from 'react'
-import ReactDOM from 'react-dom'
+import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 
 import App from './App/App'
 import SWRProvider from './App/SWRProvider'
 
-ReactDOM.render(
+const rootElement = document.querySelector('#root')
+const root = createRoot(rootElement)
+
+root.render(
   <StrictMode>
     <SWRProvider>
       <BrowserRouter>
@@ -13,5 +16,4 @@ ReactDOM.render(
       </BrowserRouter>
     </SWRProvider>
   </StrictMode>,
-  document.querySelector('#root'),
 )
