1ac73b6b117848a8fa817c7329e36ee1cde98c2c
janosbabik <janos.babik@ess.eu>
2026-06-04 18:25:51 +0200

    feat: remove SWRProvider component and simplify index.js rendering

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,17 +3,14 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 
 import App from './App/App'
-import SWRProvider from './App/SWRProvider'
 
 const rootElement = document.querySelector('#root')
 const root = createRoot(rootElement)
 
 root.render(
   <StrictMode>
-    <SWRProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </SWRProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>,
 )
